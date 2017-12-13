136b67ee09be7fed818fdf0a8519f347a85f5a6f
Dilshan Madushanka <madushankascoopz@gmail.com>
2017-12-13 14:22:27 +0530

    Revert "icon added"
    This reverts commit 220ec4ca2ac31e4e69db89f10cb09cc7377da6b7.

--- Kethmi_Holdings/frm_Main.Designer.cs
+++ Kethmi_Holdings/frm_Main.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Main));
             this.SuspendLayout();
             // 
             // frm_Main
@@ -36,7 +35,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 555);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.Name = "frm_Main";
             this.Text = "Kethmi Holdings Lands";
