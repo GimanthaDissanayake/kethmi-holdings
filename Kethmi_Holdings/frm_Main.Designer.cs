220ec4ca2ac31e4e69db89f10cb09cc7377da6b7
Dilshan Madushanka <madushankascoopz@gmail.com>
2017-12-13 14:20:43 +0530

    icon added

--- Kethmi_Holdings/frm_Main.Designer.cs
+++ Kethmi_Holdings/frm_Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Main));
             this.SuspendLayout();
             // 
             // frm_Main
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 555);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.Name = "frm_Main";
             this.Text = "Kethmi Holdings Lands";
