305f537b5db3af14411f3aef9a04f60f55cb71cb
Dilshan Madushanka <madushankascoopz@gmail.com>
2017-12-14 21:45:40 +0530

    software icon added

--- Kethmi_Holdings/frm_Login.Designer.cs
+++ Kethmi_Holdings/frm_Login.Designer.cs
@@ -42,17 +42,18 @@
             // 
             // btn_login
             // 
-            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_login.Location = new System.Drawing.Point(415, 269);
+            this.btn_login.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(427, 269);
             this.btn_login.Name = "btn_login";
             this.btn_login.Size = new System.Drawing.Size(72, 32);
             this.btn_login.TabIndex = 0;
-            this.btn_login.Text = "Login";
+            this.btn_login.Text = "&Login";
             this.btn_login.UseVisualStyleBackColor = true;
             this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Image = global::Kethmi_Holdings.Properties.Resources.kethmi;
             this.pictureBox1.Location = new System.Drawing.Point(120, 38);
             this.pictureBox1.Name = "pictureBox1";
@@ -73,44 +74,50 @@
             // 
             // tb_username
             // 
+            this.tb_username.BackColor = System.Drawing.Color.White;
+            this.tb_username.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_username.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_username.Location = new System.Drawing.Point(374, 177);
             this.tb_username.Name = "tb_username";
-            this.tb_username.Size = new System.Drawing.Size(113, 20);
+            this.tb_username.Size = new System.Drawing.Size(125, 20);
             this.tb_username.TabIndex = 4;
             this.tb_username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // tb_passsword
             // 
+            this.tb_passsword.BackColor = System.Drawing.Color.White;
+            this.tb_passsword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tb_passsword.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_passsword.Location = new System.Drawing.Point(374, 226);
             this.tb_passsword.Name = "tb_passsword";
-            this.tb_passsword.Size = new System.Drawing.Size(113, 20);
+            this.tb_passsword.Size = new System.Drawing.Size(125, 20);
             this.tb_passsword.TabIndex = 5;
             this.tb_passsword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(266, 179);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.Size = new System.Drawing.Size(86, 19);
             this.label1.TabIndex = 6;
             this.label1.Text = "Username :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(266, 225);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.Size = new System.Drawing.Size(81, 19);
             this.label2.TabIndex = 7;
             this.label2.Text = "Password :";
             // 
             // frm_Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Azure;
             this.ClientSize = new System.Drawing.Size(758, 424);
             this.Controls.Add(this.label2);
@@ -121,7 +128,9 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_login);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome";
             this.Load += new System.EventHandler(this.frm_Login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
