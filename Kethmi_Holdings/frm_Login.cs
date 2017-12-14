754708825a5fdd85c3ccca94ce138a48b6e87b80
GimanthaDissanayake <gimanthad@gmail.com>
2017-12-14 07:16:14 +0530

    changed login message

--- Kethmi_Holdings/frm_Login.cs
+++ Kethmi_Holdings/frm_Login.cs
@@ -35,7 +35,7 @@ namespace Kethmi_Holdings
         private void btn_login_Click(object sender, EventArgs e)
         {
             db.insertUpdateDelete("INSERT INTO tblUser values('something','"+tb_username.Text+"','"+tb_passsword.Text+"','manager','0766613021',0)");
-            MessageBox.Show("huraay");
+            MessageBox.Show("success");
             (new frm_Main()).Show();
         }
     }
