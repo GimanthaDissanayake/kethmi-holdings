a48f559e8f30a33743aa1c3866f65d2812d691d9
Dilshan Madushanka <madushankascoopz@gmail.com>
2017-12-14 17:59:59 +0530

    program.cs change

--- Kethmi_Holdings/Program.cs
+++ Kethmi_Holdings/Program.cs
@@ -16,7 +16,7 @@ namespace Kethmi_Holdings
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Login());
+            Application.Run(new frm_Main());
         }
     }
 }
