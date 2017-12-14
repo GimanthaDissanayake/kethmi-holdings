4c37b503a8028848ef84cea13f6f288a46480ed5
GimanthaDissanayake <gimanthad@gmail.com>
2017-12-14 10:44:05 +0530

    Project Master Form Created

---
+++ Kethmi_Holdings/frm_Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kethmi_Holdings
+{
+    public partial class frm_Project : Form
+    {
+        public frm_Project()
+        {
+            InitializeComponent();
+        }
+    }
+}
