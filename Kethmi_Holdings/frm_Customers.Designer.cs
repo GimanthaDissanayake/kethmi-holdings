1fb80f13a0867816cdf7fde7bcea7387190e5ecf
GimanthaDissanayake <gimanthad@gmail.com>
2018-05-21 10:53:37 +0530

    Workinf on frm_Main and frm_Customers

--- Kethmi_Holdings/frm_Customers.Designer.cs
+++ Kethmi_Holdings/frm_Customers.Designer.cs
@@ -40,163 +40,148 @@
             this.txt_Address = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txt_CusID = new System.Windows.Forms.TextBox();
-            this.txt_ProjectName = new System.Windows.Forms.TextBox();
             this.txt_Phone = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.cmb_ProjectName = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_CustomerList)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView_CustomerList
             // 
             this.dataGridView_CustomerList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_CustomerList.Location = new System.Drawing.Point(16, 15);
-            this.dataGridView_CustomerList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView_CustomerList.Location = new System.Drawing.Point(12, 12);
             this.dataGridView_CustomerList.Name = "dataGridView_CustomerList";
-            this.dataGridView_CustomerList.Size = new System.Drawing.Size(320, 513);
+            this.dataGridView_CustomerList.Size = new System.Drawing.Size(240, 417);
             this.dataGridView_CustomerList.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(364, 81);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(273, 66);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Customer Name";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(364, 224);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(273, 182);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 5;
             this.label5.Text = "Type";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(364, 149);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(273, 121);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.Size = new System.Drawing.Size(45, 13);
             this.label6.TabIndex = 6;
             this.label6.Text = "NIC No.";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(364, 260);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(273, 211);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(60, 17);
+            this.label11.Size = new System.Drawing.Size(45, 13);
             this.label11.TabIndex = 11;
             this.label11.Text = "Address";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(364, 188);
-            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Location = new System.Drawing.Point(273, 153);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(52, 17);
+            this.label12.Size = new System.Drawing.Size(40, 13);
             this.label12.TabIndex = 12;
             this.label12.Text = "Project";
             // 
             // txt_CusName
             // 
             this.txt_CusName.Enabled = false;
-            this.txt_CusName.Location = new System.Drawing.Point(508, 78);
-            this.txt_CusName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_CusName.Location = new System.Drawing.Point(381, 63);
             this.txt_CusName.Name = "txt_CusName";
-            this.txt_CusName.Size = new System.Drawing.Size(257, 22);
+            this.txt_CusName.Size = new System.Drawing.Size(194, 20);
             this.txt_CusName.TabIndex = 13;
             // 
             // txt_NIC
             // 
             this.txt_NIC.Enabled = false;
-            this.txt_NIC.Location = new System.Drawing.Point(508, 145);
-            this.txt_NIC.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_NIC.Location = new System.Drawing.Point(381, 118);
             this.txt_NIC.Name = "txt_NIC";
-            this.txt_NIC.Size = new System.Drawing.Size(257, 22);
+            this.txt_NIC.Size = new System.Drawing.Size(194, 20);
             this.txt_NIC.TabIndex = 14;
             // 
             // txt_Type
             // 
             this.txt_Type.Enabled = false;
-            this.txt_Type.Location = new System.Drawing.Point(508, 220);
-            this.txt_Type.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Type.Location = new System.Drawing.Point(381, 179);
             this.txt_Type.Name = "txt_Type";
-            this.txt_Type.Size = new System.Drawing.Size(257, 22);
+            this.txt_Type.Size = new System.Drawing.Size(194, 20);
             this.txt_Type.TabIndex = 15;
             // 
             // txt_Address
             // 
             this.txt_Address.Enabled = false;
-            this.txt_Address.Location = new System.Drawing.Point(508, 260);
-            this.txt_Address.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Address.Location = new System.Drawing.Point(381, 211);
             this.txt_Address.Multiline = true;
             this.txt_Address.Name = "txt_Address";
-            this.txt_Address.Size = new System.Drawing.Size(257, 62);
+            this.txt_Address.Size = new System.Drawing.Size(194, 51);
             this.txt_Address.TabIndex = 16;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(364, 44);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(273, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 17;
             this.label2.Text = "Customer ID";
             // 
             // txt_CusID
             // 
-            this.txt_CusID.Location = new System.Drawing.Point(508, 41);
-            this.txt_CusID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_CusID.Location = new System.Drawing.Point(381, 33);
             this.txt_CusID.Name = "txt_CusID";
             this.txt_CusID.ReadOnly = true;
-            this.txt_CusID.Size = new System.Drawing.Size(257, 22);
+            this.txt_CusID.Size = new System.Drawing.Size(194, 20);
             this.txt_CusID.TabIndex = 18;
-            // 
-            // txt_ProjectName
-            // 
-            this.txt_ProjectName.Enabled = false;
-            this.txt_ProjectName.Location = new System.Drawing.Point(508, 185);
-            this.txt_ProjectName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txt_ProjectName.Name = "txt_ProjectName";
-            this.txt_ProjectName.Size = new System.Drawing.Size(257, 22);
-            this.txt_ProjectName.TabIndex = 19;
             // 
             // txt_Phone
             // 
             this.txt_Phone.Enabled = false;
-            this.txt_Phone.Location = new System.Drawing.Point(508, 110);
-            this.txt_Phone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_Phone.Location = new System.Drawing.Point(381, 89);
             this.txt_Phone.Name = "txt_Phone";
-            this.txt_Phone.Size = new System.Drawing.Size(257, 22);
+            this.txt_Phone.Size = new System.Drawing.Size(194, 20);
             this.txt_Phone.TabIndex = 21;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(364, 113);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(273, 92);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 20;
             this.label3.Text = "Phone";
             // 
+            // cmb_ProjectName
+            // 
+            this.cmb_ProjectName.FormattingEnabled = true;
+            this.cmb_ProjectName.Location = new System.Drawing.Point(381, 145);
+            this.cmb_ProjectName.Name = "cmb_ProjectName";
+            this.cmb_ProjectName.Size = new System.Drawing.Size(194, 21);
+            this.cmb_ProjectName.TabIndex = 22;
+            // 
             // frm_Customers
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(789, 545);
+            this.ClientSize = new System.Drawing.Size(592, 443);
+            this.Controls.Add(this.cmb_ProjectName);
             this.Controls.Add(this.txt_Phone);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txt_ProjectName);
             this.Controls.Add(this.txt_CusID);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txt_Address);
@@ -210,7 +195,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView_CustomerList);
             this.Location = new System.Drawing.Point(82, 0);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "frm_Customers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
@@ -238,8 +222,8 @@
         private System.Windows.Forms.TextBox txt_Address;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txt_CusID;
-        private System.Windows.Forms.TextBox txt_ProjectName;
         private System.Windows.Forms.TextBox txt_Phone;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmb_ProjectName;
     }
 }
--- Kethmi_Holdings/frm_Customers.cs
+++ Kethmi_Holdings/frm_Customers.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,11 @@ namespace Kethmi_Holdings
 {
     public partial class frm_Customers : Form
     {
-        int cusID;
+        int cusID,pId;
         string strUsername = "",mode = "",strsql;
         Database db;
+
+        private string strConn = ConfigurationManager.ConnectionStrings["connstring"].ConnectionString;
 
         ButtonsStates btnStat = new ButtonsStates();
         public frm_Customers(String s)
@@ -43,15 +47,25 @@ namespace Kethmi_Holdings
         private void getCusID()
         {
             db = new Database();
-            cusID = Convert.ToInt32(db.getValue("SELECT TOP 1 cusID FROM ProjectMaster ORDER BY cusID DESC")) + 1;
+            cusID = Convert.ToInt32(db.getValue("SELECT TOP 1 cusID FROM Customer ORDER BY cusID DESC")) + 1;
+        }
+
+        private void getProjID()
+        {
+            pId = Convert.ToInt32(db.getValue("SELECT TOP 1 projID FROM ProjectMaster WHERE projName='"+cmb_ProjectName.Text+"'"));
         }
 
         private void loadData()
         {
             db = new Database();
-            strsql = "SELECT projID as 'ID',projName as 'Project Name' FROM ProjectMaster WHERE isDeleted=0";
+            strsql = "SELECT cusID as 'ID',name as 'Customer Name' FROM Customer WHERE isDeleted=0";
             dataGridView_CustomerList.DataSource = db.select(strsql);
             dataGridView_CustomerList.Enabled = true;
+        }
+
+        private void loadProjectNames()
+        {
+
         }
 
         private void clearData()
@@ -59,7 +73,7 @@ namespace Kethmi_Holdings
             txt_CusName.Clear();
             txt_NIC.Clear(); 
             txt_Phone.Clear();
-            txt_ProjectName.Clear();
+            cmb_ProjectName.Text = "";
             txt_Type.Clear();
             txt_Address.Clear();
         }
@@ -69,7 +83,7 @@ namespace Kethmi_Holdings
             txt_CusName.Enabled = value;
             txt_NIC.Enabled = value;
             txt_Phone.Enabled = value;
-            txt_ProjectName.Enabled = value;
+            cmb_ProjectName.Enabled = value;
             txt_Type.Enabled = value;
             txt_Address.Enabled = value;
         }
@@ -82,6 +96,75 @@ namespace Kethmi_Holdings
             clearData();
             enableEditing(true);
             txt_CusName.Focus();
+        }
+
+        public void ButtonEdit()
+        {
+            mode = "Edit";
+            enableEditing(true);
+            btnStat.ControlSideToolStrip(this.ParentForm, false, false, true, false, false, true);
+        }
+
+        public void ButtonClear()
+        {
+            clearData();
+        }
+
+        public void ButtonSave()
+        {
+            SqlConnection objConn = new SqlConnection(strConn);
+            objConn.Open();
+            SqlTransaction sqlTrans = objConn.BeginTransaction();
+            SqlCommand objCmd = new SqlCommand();
+            objCmd.Transaction = sqlTrans;
+            objCmd.Connection = objConn;
+
+            if(mode == "New")
+            {
+                try
+                {
+                    //get Project ID
+                    getProjID();
+
+                    //Save to Cutomers
+                    objCmd.CommandText = "INSERT INTO Customer(name,phone,nic,projID,type,address,addedUser,addedDate,changedUser,changedDate)" +
+                        " VALUES('" + txt_CusName.Text + "','" + txt_Phone.Text + "','"+txt_NIC.Text+"','"+pId+"','"+txt_Type.Text+"','"+txt_Address.Text+"',"+
+                        "'"+ strUsername + "','" + DateTime.Now + "')";
+                    objCmd.ExecuteNonQuery();
+
+                    //Commit changes 
+                    sqlTrans.Commit();
+
+                    //Clear Data Fields
+                    clearData();
+                    loadData();
+                    btnStat.ControlSideToolStrip(this.ParentForm, true, false, false, false, false, false);
+                }
+                catch (Exception ex)
+                {
+                    sqlTrans.Rollback();
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else if(mode == "Edit")
+            {
+                try
+                {
+                    //Commit changes 
+                    sqlTrans.Commit();
+
+                    //Clear Data Fields
+                    clearData();
+                    loadData();
+                    btnStat.ControlSideToolStrip(this.ParentForm, true, false, false, false, false, false);
+                }
+                catch (Exception ex)
+                {
+                    sqlTrans.Rollback();
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+
         }
     }
 }
