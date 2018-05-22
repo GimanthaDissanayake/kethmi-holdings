9301c524e7e57417b089e594ee3b23a6b1becaf1
GimanthaDissanayake <gimanthad@gmail.com>
2018-05-22 16:09:41 +0530

    update

--- Kethmi_Holdings/frm_Project.cs
+++ Kethmi_Holdings/frm_Project.cs
@@ -726,15 +726,5 @@ namespace Kethmi_Holdings
             enableEditing(true);
             btnStat.ControlSideToolStrip(this.ParentForm, false, false, true, false, false, true);
         }
-
-        private void dataGridView_projectList_KeyDown(object sender, KeyEventArgs e)
-        {
-            MessageBox.Show("Fuck down");
-        }
-
-        private void dataGridView_projectList_KeyUp(object sender, KeyEventArgs e)
-        {
-            MessageBox.Show("Fuck up");
-        }
     }
 }
